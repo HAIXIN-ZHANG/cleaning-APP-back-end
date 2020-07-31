49f62de0edd81c6139ee75bf26d34e38e328cc9e
Jessie Hou <jessie.houjinzhi@gmail.com>
2020-07-31 12:58:16 +1000

    ; to :

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const { authGuard,authGuardTradie, authGuardClient } = require("../middleware/authGuard");
 
 router.post('/',authGuardClient, addOrder);
-router.get('/;id', getOrderById);
+router.get('/:id', getOrderById);
 router.get('/',authGuard, getAllOrders);
 router.put('/:id', updateOrderById);
 router.patch('/:id/tradies/:id',authGuardTradie, updateOrderByTradie);
